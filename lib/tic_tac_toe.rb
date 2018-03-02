ac651f1a2a41889c19060016cd2549a2871c7386
Seth Wax <sethlwax@gmail.com>
2018-03-02 10:43:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.index do |combination|
+    WIN_COMBINATIONS.detect do |combination|
       combination.all?{|index| @board[index] == "X"} || combination.all?{|index| @board[index] == "O"}
     end
   end
