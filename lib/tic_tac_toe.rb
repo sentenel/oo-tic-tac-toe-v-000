e3007f1fa60b8c76031afba3cfac605fe67c8cf8
Seth Wax <sethlwax@gmail.com>
2018-03-02 10:19:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,6 +40,19 @@ class TicTacToe
   end
 
   def turn
-
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index)
+      display_board
+    else
+      turn
+    end
   end
+
+  def turn_count
+    @board.count{|marker| marker != " "}
+  end
+  
 end
