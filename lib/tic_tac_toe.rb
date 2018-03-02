5e4689a520ae43a0cd5f290e488fe4c07e3c01a1
Seth Wax <sethlwax@gmail.com>
2018-03-02 10:02:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,4 +4,15 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5]  # Middle row
+  [6,7,8]  # Bottom row
+  [0,3,6], # Left column
+  [1,4,7]  # Middle column
+  [2,5,8]  # Right column
+  [0,4,8]  # First diagonal
+  [2,4,6]  # Second diagonal
+]
+
 end
