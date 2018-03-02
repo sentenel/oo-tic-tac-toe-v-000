3779a4d52edc7ccab6191c17c51423dbaa66c195
Seth Wax <sethlwax@gmail.com>
2018-03-02 10:03:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,6 +13,14 @@ class TicTacToe
   [2,5,8], # Right column
   [0,4,8], # First diagonal
   [2,4,6], # Second diagonal
-]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
 end
